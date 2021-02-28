c3ca9630ede891c7e49c7033ecd8ad5616195378
Erik Seward <eseward34@gmail.com>
2021-02-28 13:41:39 -0800

    about page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react'
-// import { HashRouter as Router, Route } from "react-router-dom"
+import { HashRouter as Router, Route } from "react-router-dom"
 import './App.css';
-// import Login from './pages/Login';
-// import Register from "./pages/Register"
-// import Home from "./pages/Home/index"
+import Login from './pages/Login';
+import Register from "./pages/Register"
+import Home from "./pages/Home/index"
 import About from './pages/Splash';
 
 
 function App() {
   return (
-    <About/>
-
-    // <Router>
-    // <div className="App">
-    //   <Route exact path="/login" component={Login} />
-    //   <Route exact path="/register" component={Register} />
-    //   <Route exact path="/home" component={Home} />
-    // </div>
-    // </Router>
+    <Router>
+    <div className="App">
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/splash" component={About} />
+    </div>
+    </Router>
   );
 }
 
