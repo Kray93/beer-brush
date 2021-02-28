e7583cb5b11178b3dbcec0e6dbff121d7863a749
Erik Seward <eseward34@gmail.com>
2021-02-28 13:36:52 -0800

    created unresponsive about page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import { HashRouter as Router, Route } from "react-router-dom"
+// import { HashRouter as Router, Route } from "react-router-dom"
 import './App.css';
-import Login from './pages/Login';
-import Register from "./pages/Register"
-import Home from "./pages/Home/index"
+// import Login from './pages/Login';
+// import Register from "./pages/Register"
+// import Home from "./pages/Home/index"
+import About from './pages/Splash';
+
 
 function App() {
   return (
-    <Router>
-    <div className="App">
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/home" component={Home} />
-    </div>
-    </Router>
+    <About/>
+
+    // <Router>
+    // <div className="App">
+    //   <Route exact path="/login" component={Login} />
+    //   <Route exact path="/register" component={Register} />
+    //   <Route exact path="/home" component={Home} />
+    // </div>
+    // </Router>
   );
 }
 
