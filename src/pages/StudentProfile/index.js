fc42977d7b23534ba66aabbe2ac42c43a7174760
Tyler <thibault.tyler@gmail.com>
2021-03-03 21:17:50 -0800

    updated student page

--- src/pages/StudentProfile/index.js
+++ src/pages/StudentProfile/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
 import ProfileGrid from "../../components/ProfileGrid";
 import Grid from "@material-ui/core/Grid";
@@ -11,6 +11,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import axios from "axios";
 
 function createData(name, Date, Time, Instructor) {
   return { name, Date, Time, Instructor };
@@ -19,63 +20,101 @@ function createData(name, Date, Time, Instructor) {
 const selectedClasses = [];
 
 function StudentProfile() {
+  const [userData, setUserData] = useState()
+  const [view, setView] = useState("overview")
 
+  useEffect(() => {
+    let token = JSON.parse(localStorage.getItem("activeUser"));
+    token = token.data.token;
 
-
-  const artistName = () => {
-    let activeUser = JSON.parse(localStorage.getItem("activeUser"));
-    let fname = activeUser.data.user.fname;
-      if (activeUser.data.user.isArtist===true){
-        return fname;
+    axios.get(`http://localhost:3001/api/user/`, {
+      headers: {
+        "authorization": `Bearer ${token}`
       }
+    })
+      .then(resp => {
+        setUserData(resp.data.data)
+      }).catch(err => console.log(err))
+  }, [])
+
+  const changeView = (e) => {
+    let newView = e.target.parentElement.value
+    setView(newView)
+  }
+
+  const content = () => {
+    if (view === 'overview') {
+      return <div>
+        <Grid container justify='center'>
+          <h2 className='schedule' >Class Schedule</h2>
+          <TableContainer component={Paper} className='tableCont'>
+            <Table className='table' aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Class:</TableCell>
+                  <TableCell align="right">Date:</TableCell>
+                  <TableCell align="right">Time:</TableCell>
+                  <TableCell align="right">Instructor:</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {selectedClasses.map((row) => (
+                  <TableRow key={row.name}>
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.Date}</TableCell>
+                    <TableCell align="right">{row.Time}</TableCell>
+                    <TableCell align="right">{row.Instructor}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+      </div>
+    } else if (view === "addProject") {
+      return <div>
+        <form className="addProjectForm">
+          <input type="text" name="title" placeholder="title"/>
+          <input type="text" name="dateStarted" placeholder="date started"/>
+          <input type="text" name="dateFinished" placeholder="date finished"/>
+          <input type="text" name="description" placeholder="description"/>
+          <input type="text" name="materialUsed" placeholder="material used"/>
+          <input type="text" name="category" placeholder="category"/>
+          <div>
+            <label htmlFor="forSale">For Sale:</label>
+            <input type="checkbox" name="forSale" />
+          </div>
+          <div>
+            <label htmlFor="isPublic">Make it public:</label>
+            <input type="checkbox" name="forSale" />
+          </div>
+          <input type="file" name="photo" />
+          <button>Add Project!</button>
+        </form>
+      </div>
+    }
   }
 
   return (
     <div>
-      <Grid container justify='center'>
-        <div className='name'>
-            <h1>Hello, {artistName()}</h1>  
+      {userData !== undefined ?
+        <div>
+
+
+          <Grid container justify='center'>
+            <div className='name'>
+              <h1>Hello, {userData.fname}</h1>
+            </div>
+          </Grid>
+          <Button className="addBtn" variant="contained" value="addProject" onClick={changeView}>Add Project</Button>
+          <Button className="addBtn" variant="contained" value="overview" onClick={changeView}>Overview</Button>
+          {content()}
         </div>
-      </Grid>
-        <Button className="addBtn" variant="contained">Add Project</Button>
-
-      <Grid container justify='center'>
-        <h2 className='schedule' >Class Schedule</h2>
-        <TableContainer component={Paper} className='tableCont'>
-      <Table className='table' aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Class:</TableCell>
-            <TableCell align="right">Date:</TableCell>
-            <TableCell align="right">Time:</TableCell>
-            <TableCell align="right">Instructor:</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {selectedClasses.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.Date}</TableCell>
-              <TableCell align="right">{row.Time}</TableCell>
-              <TableCell align="right">{row.Instructor}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-      </Grid>
-      
-
-      <Grid container justify='center'>
-        <ProfileGrid
-        //test props hard code
-        image="../../../public/assets/images/cda.jpg"
-        title="picture"
-        description="this is a birds eye view"
-        />
-      </Grid>
+        :
+        <p>Loading...</p>
+      }
     </div>
   );
 }
